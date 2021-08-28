dc08d1bc32b06e2a8fdfb757bac4a9c3d58561fa
Pedro Germano <pedrogermano232@gmail.com>
2021-08-28 08:08:17 -0300

    aula28

---
+++ logica-programacao/aula27/index.js
@@ -0,0 +1,7 @@
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+const corUsuario = 'Pink';
+const corPadrao = corUsuario || 'Preta';
+
+console.log(nivelUsuario, corUsuario)
---
+++ logica-programacao/aula28/index.js
@@ -0,0 +1,25 @@
+// const tresHoras = 60 * 60 * 3 * 1000
+// const umDia = 60 * 60 * 24 * 1000
+// const data = new Date(0 + tresHoras - umDia);
+// const data = new Date(2019, 3)
+// const data = new Date();
+// console.log(data.toString());
+
+function zeroAEsquerda (num){
+  return num >= 10 ? num : `0${num}`;
+}
+
+function formataData (data) {
+  const dia = zeroAEsquerda(data.getDate());
+  const mes = zeroAEsquerda(data.getMonth() + 1);
+  const ano = zeroAEsquerda(data.getFullYear());
+  const hora = zeroAEsquerda(data.getHours());
+  const min = zeroAEsquerda(data.getMinutes());
+  const seg = zeroAEsquerda(data.getSeconds());
+
+  return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
